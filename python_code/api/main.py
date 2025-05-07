00490201464608c631f2ca3bc07ffccb0116c7f1
jccabarlo <jessecabarlo@gmail.com>
2025-05-07 16:14:27 +0800

    add(main.py): create main entry point for the chatbot application

---
+++ python_code/api/main.py
@@ -0,0 +1,9 @@
+from agent_controller import AgentController
+
+def main():
+    agent_controller = AgentController()
+    agent_controller.get_response()
+
+
+if __name__ == "__main__":
+    main()
