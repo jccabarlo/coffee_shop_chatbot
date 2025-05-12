4cb4066e60d347d38b77beb160e4d746072d32a1
jccabarlo <jessecabarlo@gmail.com>
2025-05-12 10:51:01 +0800

    fix(onboarding): update text styles and ensure consistent font usage

--- coffee_shop/app/index.tsx
+++ coffee_shop/app/index.tsx
@@ -18,11 +18,16 @@ export default function OnboardingScreen() {
         {/* Content Overlay View */}
         <View className="absolute bottom-0 left-0 right-0 bg-black/75 pt-12 pb-10 px-6 items-center">
           {/* Using bg-black/75 as a simplification of the Figma gradient */}
-
-          <Text className="text-white text-[32px] font-semibold text-center mb-4 leading-[48px]">
+          <Text
+            className="text-white text-[32px] font-semibold text-center mb-4 leading-[48px]"
+            style={{ fontFamily: "Author-Bold", fontSize: 32 }}
+          >
             Fall in Love with Coffee in Blissful Delight!
           </Text>
-          <Text className="text-[#A2A2A2] text-md font-normal text-center mb-10 leading-[21px]">
+          <Text
+            className="text-[#A2A2A2] text-center mb-10 leading-[21px] text-xl"
+            style={{ fontFamily: "Author-Semibold" }}
+          >
             Welcome to our cozy coffee corner, where every cup is a delightful
             for you.
           </Text>
@@ -30,8 +35,11 @@ export default function OnboardingScreen() {
             className="bg-[#C67C4E] rounded-2xl w-full py-4 items-center justify-center"
             onPress={() => router.replace("/(tabs)")}
           >
-            <Text className="text-white text-base font-semibold text-center">
-              Get Started
+            <Text
+              className="text-white text-center"
+              style={{ fontFamily: "Author-Semibold", fontSize: 20 }}
+            >
+              GET STARTED
             </Text>
           </Button>
         </View>
