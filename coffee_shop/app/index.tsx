98d61382c7eebdd848cdc336b26f4c332c549f1a
jccabarlo <jessecabarlo@gmail.com>
2025-05-12 10:57:31 +0800

    refactor(onboarding): simplify import statements and standardize font usage

--- coffee_shop/app/index.tsx
+++ coffee_shop/app/index.tsx
@@ -1,11 +1,6 @@
 import { useRouter } from "expo-router";
 import React from "react";
-import {
-  ImageBackground,
-  SafeAreaView,
-  StatusBar,
-  View,
-} from "react-native";
+import { ImageBackground, SafeAreaView, StatusBar, View } from "react-native";
 
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
@@ -24,15 +19,11 @@ export default function OnboardingScreen() {
         <View className="absolute bottom-0 left-0 right-0 bg-black/75 pt-12 pb-10 px-6 items-center">
           {/* Using bg-black/75 as a simplification of the Figma gradient */}
           <Text
-            className="text-white text-[32px] font-semibold text-center mb-4 leading-[48px]"
-            style={{ fontFamily: "Author-Bold", fontSize: 32 }}
+            className="text-white text-[32px] font-[Author-Bold] text-center mb-4 leading-[48px]"
           >
             Fall in Love with Coffee in Blissful Delight!
           </Text>
-          <Text
-            className="text-[#A2A2A2] text-center mb-10 leading-[21px] text-xl"
-            style={{ fontFamily: "Author-Semibold" }}
-          >
+          <Text className="text-[#A2A2A2] text-center mb-10 leading-[21px] text-xl font-[Author-Semibold]">
             Welcome to our cozy coffee corner, where every cup is a delightful
             for you.
           </Text>
@@ -41,8 +32,8 @@ export default function OnboardingScreen() {
             onPress={() => router.replace("/(tabs)")}
           >
             <Text
-              className="text-white text-center"
-              style={{ fontFamily: "Author-Semibold", fontSize: 20 }}
+              className="text-white text-center font-[Author-Semibold]"
+              style={{ fontSize: 16 }}
             >
               GET STARTED
             </Text>
