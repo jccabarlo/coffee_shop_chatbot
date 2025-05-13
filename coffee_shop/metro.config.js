390baebe814c231debb0acceceb96d29e38ed258
jccabarlo <jessecabarlo@gmail.com>
2025-05-14 00:29:31 +0800

    fix(metro.config): resolve iOS bundling error for @supabase/supabase-js by disabling package exports

--- coffee_shop/metro.config.js
+++ coffee_shop/metro.config.js
@@ -3,6 +3,12 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// iOS Bundling failed
+// The package at "node_modules/ws/lib/stream.js" attempted to import the Node standard library module "stream".
+// It failed because the native React runtime does not include the Node standard library.
+// set the config to fix the bundling error for @supabase/supabase-js
+config.resolver.unstable_enablePackageExports = false;
+
 // Configure SVG transformer
 config.transformer = {
   ...config.transformer,
