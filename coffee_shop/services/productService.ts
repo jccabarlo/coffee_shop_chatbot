ad65b55f5ea126a126f12461ee2ee93d87db0043
jccabarlo <jessecabarlo@gmail.com>
2025-05-16 14:43:01 +0800

    feat(products): implement getProduct function to fetch product by ID

--- coffee_shop/services/productService.ts
+++ coffee_shop/services/productService.ts
@@ -8,3 +8,16 @@ export async function getProducts() {
 
   return data;
 }
+
+export async function getProduct(id: number) {
+  const { data, error } = await supabaseClient
+    .from("products")
+    .select("*")
+    .eq("id", id);
+
+  if (!data) {
+    throw new Error("Product not found");
+  }
+
+  return data.at(0);
+}
