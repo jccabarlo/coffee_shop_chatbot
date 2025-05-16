7afd793ef3681731c92118d4c2b4e1ed17331315
jccabarlo <jessecabarlo@gmail.com>
2025-05-16 14:42:32 +0800

    feat(products): add ProductsListScreen component with data fetching

---
+++ coffee_shop/app/products/index.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import { router } from "expo-router";
+import React from "react";
+import { FlatList, Pressable, SafeAreaView, View } from "react-native";
+
+import { Button } from "@/components/ui/button";
+import { getProducts } from "@/services/productService";
+import { Text } from "~/components/ui/text";
+
+export default function ProductsListScreen() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+
+  return (
+    <SafeAreaView className="flex-1 bg-slate-100">
+      <FlatList
+        className="p-8"
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <Pressable
+              className="justify-between items-center"
+              onPress={() => {
+                router.push(`/products/${item.id}`);
+              }}
+            >
+              <Text className="font-[Author-Semibold] text-xl">
+                {item.name} {" - "}
+                <Text className="font-[Author-Regular] text-sm">
+                  {item.description}
+                </Text>
+              </Text>
+            </Pressable>
+          );
+        }}
+        ItemSeparatorComponent={() => <View className="h-6" />}
+      />
+      <View className="flex-row justify-center items-center gap-4">
+        <Button onPress={() => router.back()}>
+          <Text>Go Back</Text>
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+}
