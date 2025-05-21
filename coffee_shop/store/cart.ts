94941b08bb161aa2fe855addc7c9476f0f1226e3
jccabarlo <jessecabarlo@gmail.com>
2025-05-21 18:13:07 +0800

    feat(cart): implement cart state management with addItemToCart functionality

--- coffee_shop/store/cart.ts
+++ coffee_shop/store/cart.ts
@@ -1,11 +1,41 @@
+import { Product } from "@/types/types";
 import { create } from "zustand";
 
 interface CartState {
   orders_count: number;
   increase: (by: number) => void;
+  cart_items: Record<number, Product>;
+  addItemToCart: (item: Product) => void;
 }
 
 export const useCartStore = create<CartState>()((set) => ({
   orders_count: 0,
   increase: (by) => set((state) => ({ orders_count: state.orders_count + by })),
+  cart_items: [],
+  addItemToCart: (item) =>
+    set((state) => {
+      const cartItem = state.cart_items[item.id];
+      if (!cartItem) {
+        return {
+          cart_items: { 
+            ...state.cart_items,
+            [item.id]: { ...item, quantity: 1 } 
+          },
+        };
+      }
+
+      if (cartItem) {
+        return {
+          cart_items: {
+            ...state.cart_items,
+            [item.id]: {
+              ...cartItem,
+              quantity: cartItem.quantity + 1,
+            },
+          },
+        };
+      }
+
+      return state;
+    }),
 }));
